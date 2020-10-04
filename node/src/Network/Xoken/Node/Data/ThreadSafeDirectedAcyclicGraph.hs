b136b831455f9555583171d862199ec0e872ee7d
stoichammer <tinqnit@gmail.com>
2020-10-04 12:00:05 +0530

    concurrent DAG

--- node/src/Network/Xoken/Node/Data/ThreadSafeDirectedAcyclicGraph.hs
+++ node/src/Network/Xoken/Node/Data/ThreadSafeDirectedAcyclicGraph.hs
@@ -11,14 +11,6 @@
 
 module Network.Xoken.Node.Data.ThreadSafeDirectedAcyclicGraph
     ( TSDirectedAcyclicGraph(..)
-    , new
-    , insert
-    -- , delete
-    -- , Network.Xoken.Node.Data.ThreadSafeHashTable.lookup
-    -- , mutate
-    -- , Network.Xoken.Node.Data.ThreadSafeHashTable.mapM_
-    -- , fromList
-    -- , toList
     ) where
 
 import Control.Concurrent (threadDelay)
@@ -34,6 +26,7 @@ import Data.Hashable
 import Data.Int
 import qualified Data.List as L
 import Data.Sequence as SQ
+import Data.Set as ST
 import Data.Text as T
 import qualified Network.Xoken.Node.Data.ThreadSafeHashTable as TSH
 import Numeric as N
@@ -47,82 +40,70 @@ instance Exception DAGException
 
 data TSDirectedAcyclicGraph v =
     TSDirectedAcyclicGraph
-        { vertices :: !(TSH.TSHashTable v ())
-        , topologicalSorted :: !(MVar (Seq v))
-        , dependentsQueue :: !(TSH.TSHashTable v (MVar ()))
+        { vertices :: !(TSH.TSHashTable v v) -- mapping of vertex to head-of-Sequence
+        , topologicalSorted :: !(TSH.TSHashTable v (Seq v)) -- key : value :: head : sequence
+        , dependents :: !(TSH.TSHashTable v (MVar v)) -- 
         }
 
 new :: Int16 -> IO (TSDirectedAcyclicGraph v)
-new size = do
-    vertices <- TSH.new size
-    topoSorted <- newMVar (SQ.empty)
+new vsize = do
+    vertices <- TSH.new vsize
+    topoSorted <- TSH.new 1
     dep <- TSH.new 1
     return $ TSDirectedAcyclicGraph vertices topoSorted dep
 
-insert :: (Eq v, Hashable v) => TSDirectedAcyclicGraph v -> v -> [v] -> IO ()
-insert dag v edges = do
-    mapM_
-        (\dep -> do
-             res <- TSH.lookup (vertices dag) dep
-             case res of
-                 Just p -> return ()
-                 Nothing -> do
-                     ev <- TSH.lookup (dependentsQueue dag) dep
-                     event <-
-                         case ev of
-                             Just e -> return e
-                             Nothing -> newEmptyMVar
-                     TSH.insert (dependentsQueue dag) dep event
-                     ores <- LA.race (liftIO $ readMVar event) (liftIO $ threadDelay (60 * 1000000))
-                     case ores of
-                         Right () -> throw DependentTxNotFound
-                         Left res -> do
-                             TSH.delete (dependentsQueue dag) dep
-                             return ())
-        edges
-    modifyMVar_
-        (topologicalSorted dag)
-        (\seq -> do
-             TSH.insert (vertices dag) v ()
-             return $ seq |> v)
-    return ()
+coalesce :: (Eq v, Hashable v, Ord v) => TSDirectedAcyclicGraph v -> v -> [v] -> IO ()
+coalesce dag vt edges = do
+    vals <-
+        mapM
+            (\dep -> do
+                 res <- TSH.lookup (vertices dag) dep
+                 case res of
+                     Just indx -> return indx -- do multi level recursive lookup
+                     Nothing -> return dep)
+            edges
+    let head = vals !! 0
+    if L.all (\x -> x == head) vals
+        then do
+            seq <- TSH.lookup (topologicalSorted dag) (head)
+            case seq of
+                Just sq -> do
+                    etry <- TSH.lookup (vertices dag) vt
+                    case etry of
+                        Just x -> do
+                            TSH.insert (topologicalSorted dag) head (sq <> x)
+                        Nothing -> do
+                            TSH.insert (topologicalSorted dag) head (sq |> vt)
+                            TSH.insert (vertices dag) vt head
+                    event <- TSH.lookup (dependents dag) vt
+                    case event of
+                        Just ev -> liftIO $ putMVar ev head -- value in MVar, vt or head?
+                        Nothing -> return ()
+                Nothing
+                    -- TSH.insert (vertices dag) vt vt -- needed?
+                 -> do
+                    TSH.insert (topologicalSorted dag) vt (SQ.singleton vt)
+        else do
+            TSH.insert (topologicalSorted dag) vt (SQ.singleton vt) -- needed?
+            -- TSH.insert (vertices dag) vt vt -- needed?
+            par <-
+                mapM
+                    (\dep -> do
+                         ev <- TSH.lookup (dependents dag) dep
+                         event <-
+                             case ev of
+                                 Just e -> return e
+                                 Nothing -> newEmptyMVar
+                         TSH.insert (dependents dag) dep event
+                         ores <- LA.race (liftIO $ readMVar event) (liftIO $ threadDelay (60 * 1000000))
+                         case ores of
+                             Right () -> throw DependentTxNotFound
+                             Left res -> do
+                                 return res)
+                    vals
+            let uniq = ST.toList $ ST.fromList par
+            coalesce dag vt uniq
 --
--- insertTry ::
--- getNextVertex ::
--- toList ::
--- take ::
 --
---    
--- delete :: (Eq k, Hashable k) => TSHashTable k v -> k -> IO ()
--- delete tsh k = do
---     let index = (hash k) `mod` (fromIntegral $ size tsh)
---     withMVar ((hashTableList tsh) !! index) (\hx -> H.delete hx k)
--- lookup :: (Eq k, Hashable k) => TSHashTable k v -> k -> IO (Maybe v)
--- lookup tsh k = do
---     let index = (hash k) `mod` (fromIntegral $ size tsh)
---     withMVar ((hashTableList tsh) !! index) (\hx -> H.lookup hx k)
--- mutate :: (Eq k, Hashable k) => TSHashTable k v -> k -> (Maybe v -> (Maybe v, a)) -> IO a
--- mutate tsh k f = do
---     v <- Network.Xoken.Node.Data.ThreadSafeHashTable.lookup tsh k
---     case f v of
---         (Nothing, a) -> do
---             delete tsh k
---             return a
---         (Just v, a) -> do
---             insert tsh k v
---             return a
--- mapM_ :: ((k, v) -> IO a) -> TSHashTable k v -> IO ()
--- mapM_ f tsh = do
---     traverse (\ht -> liftIO $ withMVar ht (\hx -> H.mapM_ f hx)) (hashTableList tsh)
---     return ()
--- toList :: (Eq k, Hashable k) => TSHashTable k v -> IO [(k, v)]
--- toList tsh = fmap L.concat $ mapM (\x -> withMVar x (\hx -> H.toList hx)) (hashTableList tsh)
--- fromList :: (Eq k, Hashable k) => Int16 -> [(k, v)] -> IO (TSHashTable k v)
--- fromList size kv = do
---     tsh <- new size
---     traverse (\(k, v) -> insert tsh k v) kv
---     return tsh
--- withTSHLock :: (Eq k, Hashable k) => TSHashTable k v -> k -> (k -> IO ()) -> IO ()
--- withTSHLock tsh k func = do
---     let index = (hash k) `mod` (fromIntegral $ size tsh)
---     withMVar ((hashTableList tsh) !! index) (\hx -> func k)
+--
+--            
